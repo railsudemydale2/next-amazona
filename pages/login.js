db72e9100471007c04653692fe86139f75d5ce34
dale <stewartdale45@yahoo.com>
2021-08-18 16:13:40 -0400

    Built login api and installed jwt to generate token for user login and in login.js I used axios to make AJAX requests to backendd to authenticate user and also defined a post method to authenticate a user in login.js and generate a token for the user.If the user email or password is wrong we show a message prompt indicating that user and password are the wrong credentials

--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,36 @@
 import {
-  Button,
   List,
   ListItem,
-  TextField,
   Typography,
+  TextField,
+  Button,
   Link,
 } from '@material-ui/core';
+import axios from 'axios';
 import NextLink from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import useStyles from '../utils/styles';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const classes = useStyles();
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post('/api/users/login', {
+        email,
+        password,
+      });
+      alert('succss login');
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message);
+    }
+  };
   return (
-    <Layout>
-      <form formName={classes.form}>
+    <Layout title="Login">
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component="h1" variant="h1">
           Login
         </Typography>
@@ -27,16 +42,26 @@ export default function Login() {
               id="email"
               label="Email"
               inputProps={{ type: 'email' }}
+              onChange={(e) => setEmail(e.target.value)}
             ></TextField>
           </ListItem>
-
+          <ListItem>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="password"
+              label="Password"
+              inputProps={{ type: 'password' }}
+              onChange={(e) => setPassword(e.target.value)}
+            ></TextField>
+          </ListItem>
           <ListItem>
             <Button variant="contained" type="submit" fullWidth color="primary">
               Login
             </Button>
           </ListItem>
           <ListItem>
-            Don't have an account? &nbsp{' '}
+            Don't have an account? &nbsp;
             <NextLink href="/register" passHref>
               <Link>Register</Link>
             </NextLink>
