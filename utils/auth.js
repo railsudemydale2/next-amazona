05c3b7cf46b986984c101eb4ae16a5e1570fc990
dale <stewartdale45@yahoo.com>
2021-08-20 17:03:05 -0400

    I created a model to save order information including order items, shipping info and payment method in place order. I have also used a circular progress to show progress bar while the user is creating an order and used a snack bar to show error messages and also be implemented, place order handler is sent and AJAX requests to this API to create an order.In the pages/api/orders/index.js I created a post method to create an order in the database and also be createderror.js utility file to format error messages and also to handle middleware for error in the next connect in the backend

--- utils/auth.js
+++ utils/auth.js
@@ -8,10 +8,29 @@ const signToken = (user) => {
       email: user.email,
       isAdmin: user.isAdmin,
     },
+
     process.env.JWT_SECRET,
     {
       expiresIn: '30d',
     }
   );
 };
-export { signToken };
+const isAuth = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (authorization) {
+    // Bearer xxx => xxx
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: 'Token is not valid' });
+      } else {
+        req.user = decode;
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({ message: 'Token is not suppiled' });
+  }
+};
+
+export { signToken, isAuth };
